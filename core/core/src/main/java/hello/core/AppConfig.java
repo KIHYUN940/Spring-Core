118aabfe2f267d3b317f2394425debaeacb7c01b
kihyun940 <houng027@gmail.com>
2023-12-29 14:35:37 +0900

    refactor: 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 - AppConfig 리팩토링
    -- AppConfig를 보면 중복이 있고, 역할에 따른 구현이 잘 안보임
    
    new MemoryMemberRepository()` 이 부분이 중복 제거되었다. 이제`MemoryMemberRepository` 를 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
    
    `AppConfig` 를 보면 역할과 구현 클래스가 한눈에 들어온다. 애플리케이션 전체 구성이 어떻게 되어있는지 빠르게 파악할 수 있다.

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -10,11 +11,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해 객체가 new 인스턴스에 들어간다해서 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자를 통해 객체가 new 인스턴스에 들어간다해서 생성자 주입
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
