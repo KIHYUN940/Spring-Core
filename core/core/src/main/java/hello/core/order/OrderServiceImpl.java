59cd86d55b5be51db70f989e6ca49701b10b883e
kihyun940 <houng027@gmail.com>
2024-01-06 18:20:48 +0900

    feat: 컴포넌트 스캔 - 컴포넌트 스캔과 의존관계 자동 주입 시작하기
    스프링은 설정 정보가 없어도 자동으로 스프링 빈을 등록하는 컴포넌트 스캔이라는 기능을 제공한다.
    의존관계도 자동으로 주입하는 `@Autowired` 라는 기능도 제공한다.
    
    - 컴포넌트 스캔을 사용하려면 먼저 `@ComponentScan` 을 설정 정보에 붙여주면 된다.
    ㄴ 컴포넌트 스캔은 이름 그대로 `@Component` 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다.
    
    - `@Autowired` 는 의존관계를 자동으로 주입해준다

--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -13,6 +16,7 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository; // final이 붙어 있으면 무조건 생성자를 통해 할당 해야함
     private final DiscountPolicy discountPolicy; // 인터페이스에 의존 / DIP 지킴
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
