f3e071b1d204d8cd93ea6d28eda54c4d7f85b705
kihyun940 <houng027@gmail.com>
2024-01-09 17:48:35 +0900

    feat: 의존관계 자동 주입 - 생성자 주입을 선택해라!
    * 최근에는 스프링을 포함한 DI 프레임워크 대부분이 생성자 주입을 권장
    
    **불변**
    대부분의 의존관계 주입은 한번 일어나면 애플리케이션 종료시점까지 의존관계를 변경할 일이 없다. 오히려 대부분의 의존관계는 애플리케이션 종료 전까지 변하면 안된다.(불변해야 한다.)
    
    *누락*
    생성자 주입을 사용하면 주입 데이터를 누락 했을 때 컴파일 오류가 발생한다.
    그리고 IDE에서 바로 어떤 값을 필수로 주입해야 하는지 알 수 있다.
    
    **final 키워드**
    생성자 주입을 사용하면 필드에 `final` 키워드를 사용할 수 있다. 그래서 생성자에서 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다.
    
    ** 수정자 주입을 포함한 나머지 주입 방식은 모두 생성자 이후에 호출되므로, 필드에 `final` 키워드를 사용할 수 없다. 오직 생성자 주입 방식만 `final` 키워드를 사용할 수 있다.

--- core/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,10 +13,21 @@ public class OrderServiceImpl implements OrderService {
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy(); // OCP 위반
 
-    private final MemberRepository memberRepository; // final이 붙어 있으면 무조건 생성자를 통해 할당 해야함
+    private final MemberRepository memberRepository; // ** final이 붙어 있으면 무조건 생성자를 통해 할당 해야함 / 생성자에 혹시라도 값이 설정되지 않는 오류를 컴파일 시점에 막아준다.
     private final DiscountPolicy discountPolicy; // 인터페이스에 의존 / DIP 지킴
 
-    @Autowired //생성자가 하나일 땐 생략 가능
+// setter 주입 방식 테스트
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+        @Autowired //생성자가 하나일 땐 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
